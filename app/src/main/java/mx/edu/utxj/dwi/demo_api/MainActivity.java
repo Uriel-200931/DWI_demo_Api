8dbf74ba17c9899e3eeb5be5960727f6441c5ee4
uriel-200931 <uriel.maldonadocortez@utxicotepec.edu.mx>
2023-05-30 16:03:02 -0600

    METHOD MAIN

--- app/src/main/java/mx/edu/utxj/dwi/demo_api/MainActivity.java
+++ app/src/main/java/mx/edu/utxj/dwi/demo_api/MainActivity.java
@@ -2,11 +2,22 @@ package mx.edu.utxj.dwi.demo_api;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnGuardar  ;
@@ -21,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText etPrecioVenta;
     private EditText etExistencias;
     private ListView lvProductos;
+    private RequestQueue requestQueue;
+    private JsonArrayRequest jsonArrayRequest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,12 +49,38 @@ public class MainActivity extends AppCompatActivity {
         etPrecioVenta =findViewById(R.id.etPrecioVenta);
         etExistencias =findViewById(R.id.etExistencias);
         lvProductos =findViewById(R.id.lvProductos);
-
+        requestQueue= Volley.newRequestQueue(this);
         btnGuardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                ListarProductos();
             }
         });
+
+        }
+    protected void ListarProductos(){
+        String url="http://10.10.62.17:3300/";
+        jsonArrayRequest=new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Toast.makeText(MainActivity.this, response.toString(),Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, error.getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+
+        );
+
+        requestQueue.add(jsonArrayRequest);
     }
+
 }
