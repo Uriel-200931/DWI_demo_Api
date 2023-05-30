d0987032701293cf578af2944827b429f5dfde63
Uriel-200931 <uriel.maldonadocortez@utxicotepec.edu.mx>
2023-05-29 19:09:28 -0600

    main activity btn search

--- app/src/main/java/mx/edu/utxj/dwi/demo_api/MainActivity.java
+++ app/src/main/java/mx/edu/utxj/dwi/demo_api/MainActivity.java
@@ -3,13 +3,21 @@ package mx.edu.utxj.dwi.demo_api;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    private Button btnGuardar  ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        btnGuardar= findViewById(R.id.btnBuscar);
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
