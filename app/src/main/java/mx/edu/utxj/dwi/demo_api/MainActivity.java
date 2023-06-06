fdae1a27289e8cd7ab3adddc4983e236ccef1973
uriel-200931 <uriel.maldonadocortez@utxicotepec.edu.mx>
2023-06-05 18:44:44 -0600

    method get with codigobarras

--- app/src/main/java/mx/edu/utxj/dwi/demo_api/MainActivity.java
+++ app/src/main/java/mx/edu/utxj/dwi/demo_api/MainActivity.java
@@ -81,6 +81,44 @@ public class MainActivity extends AppCompatActivity {
         requestQueue= Volley.newRequestQueue(this);
         lvProducts=findViewById(R.id.lvProducts);
         listProducts();
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                JsonObjectRequest peticion = new JsonObjectRequest(
+                        Request.Method.GET,
+                        url + etCodigoBarras.getText().toString(),
+                        null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                if (response.has("status"))
+                                    Toast.makeText(MainActivity.this, "PRODUCTO NO ENCONTRADO", Toast.LENGTH_SHORT).show();
+                                else {
+                                    try {
+                                        etDescripcion.setText(response.getString("descripcion"));
+                                        etMarca.setText(response.getString("marca"));
+                                        etprecioCompra.setText(String.valueOf(response.getInt("preciocompra")));
+                                        etprecioVenta.setText(String.valueOf(response.getInt("precioventa")));
+                                        etExistencias.setText(String.valueOf(response.getInt("existencias")));
+                                    } catch (JSONException e) {
+                                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                );
+                requestQueue.add(peticion);
+            }
+        });
+
+
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -135,11 +173,11 @@ public class MainActivity extends AppCompatActivity {
                         //Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
                         for(int i = 0;i < response.length(); i++){
                             try {
+                                String codigobarras =response.getJSONObject(i).getString("codigobarras");
                                 String descripcion = response.getJSONObject(i).getString("descripcion") ;
                                 String marca = response.getJSONObject(i).getString("marca");
-                                origenDatos.add(descripcion+"::"+marca);
+                                origenDatos.add(codigobarras +"::" +descripcion+"::"+marca);
                             } catch (JSONException e) {
-                                throw new RuntimeException(e);
                             }
                         }
                         adapter = new ArrayAdapter<>(MainActivity.this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, origenDatos);
